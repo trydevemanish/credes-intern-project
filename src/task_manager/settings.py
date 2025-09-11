9cccac65ed80fe135263de509fa6f7da748894a4
manish sharma dev <manishvsharma1@gmail.com>
2025-09-11 17:19:30 +0530

    register user view

--- src/task_manager/settings.py
+++ src/task_manager/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'users',
     'tasks',
     'comments',
@@ -100,6 +101,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 
 # Internationalization
--- src/users/views.py
+++ src/users/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from rest_framework import status
+from django.http import JsonResponse
+from .serializers import UserSerializers
+from rest_framework.decorators import api_view
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+
+
+User = get_user_model()
 
 # Create your views here.
-def RegisterUser():
-    ...
+
+@csrf_exempt
+@api_view(['POST'])
+def RegisterUser(request):
+    if request.method == 'POST':
+        serializer = UserSerializers(data=request.data)
+        if serializer.is_valid():
+            checkUserExit = User.objects.filter(email=serializer.data.email).first()
+            if checkUserExit:
+                print('Email already exits!')
+                return JsonResponse({'message':'Email alreay exits!'},status=status.HTTP_400_BAD_REQUEST)
+            else:
+                serializer.save()
+                return JsonResponse({'message':'User registerd'},status=status.HTTP_201_CREATED)
+        else:
+            return JsonResponse({'message':'Data is not valid'},status=status.HTTP_400_BAD_REQUEST)
+        
+    return JsonResponse({'message':'Req method is not suitable.'},status=status.HTTP_400_BAD_REQUEST)
 
 
 def LoginUser():
